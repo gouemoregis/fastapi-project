ed75e9e1aceef6e7d22b9383123db28fd5662895
gouemoregis <gouemoregis@gmail.com>
2025-11-21 16:27:56 +0100

    Deploy to Heroku CI/CD

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello fastapi successfully deployed"}
+    return {"message": "Hello fastapi successfully deployed!"}
